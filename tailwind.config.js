d73db8b715de2bacc11d90afa3c6aa3aad31e4c8
retsofcm <louise.mcconnell@gmail.com>
2025-04-24 00:00:59 +0100

    adding tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+import aspectRatio from '@tailwindcss/aspect-ratio'
+
+export default {
+  content: [
+    './pages/**/*.{js,ts,jsx,tsx}',  // This includes all files in the pages directory
+    './components/**/*.{js,ts,jsx,tsx}',  // This includes all files in the components directory
+    './app/**/*.{js,ts,jsx,tsx}',  // If you're using the `app` directory (Next.js 13+)
+    './layouts/**/*.{js,ts,jsx,tsx}',  // If you have layouts directory
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [aspectRatio],
+}
