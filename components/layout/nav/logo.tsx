62b5f1bc71d088b4196c6d029a6815afa2b9fbee
Louise Foster <louise.mcconnell@gmail.com>
2025-05-08 21:53:55 +0100

    Update logo.tsx

--- components/layout/nav/logo.tsx
+++ components/layout/nav/logo.tsx
@@ -23,10 +23,10 @@ export const Logo = ({ className }: { className?: string }) => (
     <path fill="#165016" fillOpacity={1} stroke="none" d="M479.59 208.615c-131.52-87.231-300.046-8.12-461.072-48.284 113.23 78.756 317.704 10.034 461.072 48.284Z" />
     <path fill="green" fillOpacity={1} stroke="none" d="M484.98 202.23c114.072-168.715 279.102-51.823 380.688-80.275-87.916 82.581-219.884-22.265-380.687 80.276Z" />
     <path fill="#165016" fillOpacity={1} stroke="none" d="M483.408 208.615c129.271-87.231 294.915-8.12 453.187-48.284-111.294 78.756-312.271 10.034-453.187 48.284z" />
-    <text x="16.73" y="296.665" transform="scale(1.02882 .972)" style={{ fontFamily: 'Montserrat', fontWeight: 700, fontSize: '100.335px', fill: 'green' }}>
+    <text fill="green" x="16.73" y="296.665" transform="scale(1.02882 .972)" style={{ fontFamily: 'Montserrat', fontWeight: 700, fontSize: '100.335px' }} className={className}>
       STENSON FIELDS
     </text>
-    <text x="17.903" y="414.721" transform="scale(1.1726 .8528)" style={{ fontFamily: 'Angelina', fontSize: '132.633px', fill: '#b3b3b3' }}>
+    <text fill="#b3b3b3" x="17.903" y="414.721" transform="scale(1.1726 .8528)" style={{ fontFamily: 'Angelina', fontSize: '132.633px' }} className={className}>
       christian fellowship
     </text>
     <path fill="#4a0" fillOpacity={1} stroke="none" d="M484.156 190.402c46.085-164.01 193-122.946 263.657-169.026-47.055 87.454-167.157 36.69-263.657 169.026Z" />
