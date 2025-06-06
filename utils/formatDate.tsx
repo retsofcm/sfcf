890b052f3c1b97286ff5900fb1de78f3a8bdc662
retsofcm <louise.mcconnell@gmail.com>
2025-06-06 17:18:12 +0100

    fixed issue with BST/GMT in event times

--- utils/formatDate.tsx
+++ utils/formatDate.tsx
@@ -1,38 +1,41 @@
-import { format } from "date-fns";
+import { formatInTimeZone } from "date-fns-tz";
+
+const timeZone = "Europe/London";
 
 export function formatDateRange(startDate?: string | Date, endDate?: string | Date) {
-  // Check if startDate is valid
   if (!startDate) return "Date TBC";
 
-  // Ensure startDate is a Date instance, if not, create one
   const start = startDate instanceof Date ? startDate : new Date(startDate);
-
-  // If endDate is provided, make sure it's a valid Date, otherwise leave it undefined
   const end = endDate ? (endDate instanceof Date ? endDate : new Date(endDate)) : undefined;
 
   const formatTime = (date: Date) => {
-    const hour = format(date, "h");
-    const minutes = format(date, "mm");
-    const ampm = format(date, "a").toLowerCase();
+    const hour = formatInTimeZone(date, timeZone, "h");
+    const minutes = formatInTimeZone(date, timeZone, "mm");
+    const ampm = formatInTimeZone(date, timeZone, "a").toLowerCase();
     return minutes === "00" ? `${hour}${ampm}` : `${hour}:${minutes}${ampm}`;
   };
 
-  if (!end) return `${format(start, "MMMM do")}, ${formatTime(start)}`;
+  if (!end) return `${formatInTimeZone(start, timeZone, "MMMM do")}, ${formatTime(start)}`;
 
-  const sameDay = format(start, "yyyy-MM-dd") === format(end, "yyyy-MM-dd");
-  const sameMonth = format(start, "yyyy-MM") === format(end, "yyyy-MM");
+  const sameDay =
+    formatInTimeZone(start, timeZone, "yyyy-MM-dd") ===
+    formatInTimeZone(end, timeZone, "yyyy-MM-dd");
+
+  const sameMonth =
+    formatInTimeZone(start, timeZone, "yyyy-MM") ===
+    formatInTimeZone(end, timeZone, "yyyy-MM");
 
   if (sameDay) {
-    return `${format(start, "MMMM do")}, ${formatTime(start)}-${formatTime(end)}`;
+    return `${formatInTimeZone(start, timeZone, "MMMM do")}, ${formatTime(start)}-${formatTime(end)}`;
   }
 
   if (sameMonth) {
     const startTime = formatTime(start);
     const endTime = formatTime(end);
-    return `${format(start, "MMMM do")}-${format(end, "do")}, ${startTime}${startTime === endTime ? "" : `-${endTime}`}`;
+    return `${formatInTimeZone(start, timeZone, "MMMM do")}-${formatInTimeZone(end, timeZone, "do")}, ${startTime}${startTime === endTime ? "" : `-${endTime}`}`;
   }
 
   const startTime = formatTime(start);
   const endTime = formatTime(end);
-  return `${format(start, "MMMM do")}-${format(end, "MMMM do")}, ${startTime}${startTime === endTime ? "" : `-${endTime}`}`;
+  return `${formatInTimeZone(start, timeZone, "MMMM do")}-${formatInTimeZone(end, timeZone, "MMMM do")}, ${startTime}${startTime === endTime ? "" : `-${endTime}`}`;
 }
